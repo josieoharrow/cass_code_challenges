4580db0363837e95eb38e8016dbbd444defe517d
Josie O'Harrow <josieoharrow@hotmail.com>
2019-07-13 16:04:06 -0700

    add file

---
+++ code.rb
@@ -0,0 +1,36 @@
+s='
+t=Time.now
+def j(s)
+b = 0
+while b<150000
+f=File.open("f/"+rand.to_s,"w")
+f.puts("s='"+s+"'"+s)
+f.close
+b=b+1 
+end
+end
+Dir.mkdir("f")
+threads=[]
+7.times{
+  threads<<Thread.new{j(s)}
+}
+threads.each(&:join)
+p(Time.now-t)
+'
+t=Time.now
+def j(s)
+b = 0
+while b<150000
+f=File.open("f/"+rand.to_s,"w")
+f.puts("s='"+s+"'"+s)
+f.close
+b=b+1 
+end
+end
+Dir.mkdir("f")
+threads=[]
+7.times{
+  threads<<Thread.new{j(s)}
+}
+threads.each(&:join)
+p(Time.now-t)
