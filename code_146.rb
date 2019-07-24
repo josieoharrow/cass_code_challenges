4ed351716861a1c77e3bfdabbb519be79bd67e4e
Josie O'Harrow <josieoharrow@hotmail.com>
2019-07-24 14:57:21 -0700

    updates

--- code_146.rb
+++ code_146.rb
@@ -1,11 +1,8 @@
 s='
-t=Time.now
 def j(s)
-b = 0
+a,b=[*("A".."Z")].sample(4).join,0
 while b<150000
-f=File.open("f/"+rand.to_s,"w")
-f.puts("s='"+s+"'"+s)
-f.close
+File.open("f/"+a+b.to_s,"w") {|f| f.write(s) }
 b=b+1 
 end
 end
@@ -15,22 +12,17 @@ threads=[]
   threads<<Thread.new{j(s)}
 }
 threads.each(&:join)
-p(Time.now-t)
 '
-t=Time.now
 def j(s)
-b = 0
+a,b=[*("A".."Z")].sample(4).join,0
 while b<150000
-f=File.open("f/"+rand.to_s,"w")
-f.puts("s='"+s+"'"+s)
-f.close
+File.open("f/"+a+b.to_s,"w") {|f| f.write(s) }
 b=b+1 
 end
 end
 Dir.mkdir("f")
-threads=[]
+t=[]
 7.times{
-  threads<<Thread.new{j(s)}
+  t<<Thread.new{j(s)}
 }
-threads.each(&:join)
-p(Time.now-t)
+t.each(&:join)
