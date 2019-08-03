03947424e09950c30b1094cbe814febfe94c9b7b
Josie O'Harrow <josieoharrow@hotmail.com>
2019-08-02 22:21:13 -0700

    shorten it up :)

--- single.cr
+++ single.cr
@@ -1,18 +1,12 @@
 s=%(
-Dir.mkdir("b")
 b=0
 while b<750000
-    f = File.new "b/"+b.to_s+".cr", "w"
-    f.puts "s=%("+s+")"+s
-    f.close
-    b=b+1 
+File.write(b.to_s+".cr","s=%("+s+")"+s)
+b=b+1
 end
 )
-Dir.mkdir("b")
 b=0
 while b<750000
-    f = File.new "b/"+b.to_s+".cr", "w"
-    f.puts "s=%("+s+")"+s
-    f.close
-    b=b+1
+File.write(b.to_s+".cr","s=%("+s+")"+s)
+b=b+1
 end
